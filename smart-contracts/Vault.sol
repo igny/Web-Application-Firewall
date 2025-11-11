50671662517e0ab4ba7c04abea926d0d94a3fd75
igny <31275935+igny@users.noreply.github.com>
2025-11-11 14:17:11 +0700

    feat: wire up message broker publisher in Vault.sol

--- smart-contracts/Vault.sol
+++ smart-contracts/Vault.sol
@@ -58,3 +58,4 @@ contract EnterpriseYieldVault is ReentrancyGuard, Ownable {
 // Optimized logic batch 6455
 // Optimized logic batch 4938
 // Optimized logic batch 5149
+// Optimized logic batch 9467
